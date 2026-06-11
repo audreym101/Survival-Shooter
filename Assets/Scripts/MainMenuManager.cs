e7ffe910ef3d14f23aa8f19c071ca7ff4ce6e56e
Yvantrey <rugambayvan1@gmail.com>
2026-06-11 10:33:30 +0300

    wire up restart and main menu buttons in EndGamePanel

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [Header("Panels")]
+    [SerializeField] GameObject mainPanel;
+    [SerializeField] GameObject leaderboardPanel;
+
+    [Header("Leaderboard")]
+    [SerializeField] TMP_Text leaderboardText;
+
+    void Start() => ShowMain();
+
+    void ShowMain()
+    {
+        mainPanel.SetActive(true);
+        leaderboardPanel.SetActive(false);
+    }
+
+    public void OnPlayPressed() => SceneManager.LoadScene("GameScene");
+
+    public void OnLeaderboardPressed()
+    {
+        mainPanel.SetActive(false);
+        leaderboardPanel.SetActive(true);
+        LoadLeaderboard();
+    }
+
+    public void OnCloseLeaderboardPressed() => ShowMain();
+
+    public void OnQuitPressed()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+    void LoadLeaderboard()
+    {
+        if (LeaderboardManager.Instance == null)
+        {
+            leaderboardText.text = "No scores yet";
+            return;
+        }
+
+        var entries = LeaderboardManager.Instance.GetLeaderboardEntries();
+        if (entries.Count == 0)
+        {
+            leaderboardText.text = "No scores yet";
+            return;
+        }
+
+        leaderboardText.text = "";
+        for (int i = 0; i < entries.Count; i++)
+            leaderboardText.text += $"{i + 1}. {entries[i]}\n";
+    }
+}
